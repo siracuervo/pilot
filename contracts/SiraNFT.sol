ac38edb7961f863e34ca9af60ae2451159b67483
siracuervo <fl.siracusa@gmail.com>
2022-08-14 21:49:43 -0300

    Charge 0.1 ETH if it ain't  owner's first token

--- contracts/SiraNFT.sol
+++ contracts/SiraNFT.sol
@@ -7,13 +7,21 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract SiraFT is ERC721 {
     uint256 token_count;
     uint256 public totalSupply = 1000000;
- 
+    uint256 price = 100000000000000000;
+
     constructor() ERC721("SiraNFT", "SNFT") {}
 
     function mintNFT(address to) public
     {
         require(token_count < totalSupply, "Max supply");
+        if (balanceOf(msg.sender) != 0) {
+            require(msg.value >= price, "Not enough ETH sent; check price!");
+        }
         token_count  += 1;
         _mint(to, token_count);
+    }
+
+    function setPrice(uint256 _price) public onlyOwner {
+        price = _price;
     }
 }
